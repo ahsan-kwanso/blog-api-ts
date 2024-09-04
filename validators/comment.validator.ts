e6355f5ba8466ba20223238a5ca4fca7c14bcf35
ahsan-kwanso <m.ahsan@kwanso.com>
2024-09-04 10:41:27 +0500

    +ve int validation added

--- validators/comment.validator.ts
+++ validators/comment.validator.ts
@@ -1,7 +1,7 @@
 import { body, query, param } from "express-validator";
 // Create Comment Validation Rules
 const createCommentValidationRules = [
-  body("PostId").isInt().withMessage("Valid PostId is required"),
+  body("PostId").isInt({ gt: 0 }).withMessage("Valid PostId is required"),
   body("title").optional().trim().notEmpty().withMessage("Title is required"),
   body("content")
     .trim() // Remove leading and trailing spaces
@@ -13,19 +13,21 @@ const createCommentValidationRules = [
 
 // Update Comment Validation Rules
 const updateCommentValidationRules = [
-  param("comment_id").isInt().withMessage("Valid comment Id is required"),
+  param("comment_id").isInt({ gt: 0 }).withMessage("Valid comment Id is required"),
   body("title").optional().notEmpty().withMessage("Title cannot be empty"),
   body("content").optional().notEmpty().withMessage("Content cannot be empty"),
 ];
 
 // Delete Comment Validation Rules
-const deleteCommentValidationRules = [param("comment_id").isInt().withMessage("Valid comment ID is required")];
+const deleteCommentValidationRules = [param("comment_id").isInt({ gt: 0 }).withMessage("Valid comment ID is required")];
 
-const getCommentByIdValidationRules = [param("comment_id").isInt().withMessage("Valid comment ID is required")];
+const getCommentByIdValidationRules = [param("comment_id").isInt({ gt: 0 }).withMessage("Valid comment ID is required")];
 
-const getCommentByPostIdValidationRules = [param("post_id").isInt().withMessage("Valid post ID is required")];
+const getCommentByPostIdValidationRules = [param("post_id").isInt({ gt: 0 }).withMessage("Valid post ID is required")];
 
 const searchByTitleOrContentValidator = [
+  query("page").optional().isInt({ gt: 0 }).withMessage("Valid page is required"),
+  query("limit").optional().isInt({ gt: 0 }).withMessage("Valid limit is required"),
   query("title").optional().isString().withMessage("Title must be a string"),
   query("content").optional().isString().withMessage("Content must be a string"),
 ];
