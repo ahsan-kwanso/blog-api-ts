483987af2a5b42f02128bad1875f256d8da735ba
ahsan-kwanso <m.ahsan@kwanso.com>
2024-09-05 11:56:11 +0500

    check server

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-import express, {Express} from "express";
+import express, { Express } from "express";
 import router from "./routes/index.ts";
 import cors from "cors";
 import { PORT } from "./utils/settings.ts";
 
-const app : Express = express();
+const app: Express = express();
+app.get("/", (req, res) => {
+  res.send("Server Started!");
+});
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cors());
 app.use(router);
